60cc88ffee991b2408d0a685cabc37fbd1bdd671
Oscar Coghlan <oscar.coghlan@gmail.com>
2023-06-13 11:00:25 +1000

    Hello world

--- main.cpp
+++ main.cpp
@@ -4,11 +4,39 @@
 
 
 int main() {
+
+	GLFWwindow* window;
+
 	if (!glfwInit())
 	{
-		// Initialization failed
-		std::cout << "AH";
+		return -1;
 	}
+
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);	
+
+	if (!window) {
+        glfwTerminate();
+        return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+
+
+	while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+
+
 
 	glfwTerminate();
 
